9aabae13b7c94bae9f4e45d7b58e85de9547e7ce
Tymofii Matviienko <tymofii.matviienko.2@student.tuke.sk>
2025-02-06 18:32:29 +0100

    Add Version 0.0.2

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ import psutil
 import speech_recognition
 import json
 import webbrowser
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('voice', voices[2].id)
 
 sr = speech_recognition.Recognizer()
 sr.pause_threshold = 0.5
 
 with open('commands/commands.json', 'r', encoding='utf-8') as file:
     commands_dict = json.load(file)
+
+def speak(text):
+    """Озвучивает текст"""
+    engine.say(text)
+    engine.runAndWait()
 
 def listen_command():
     """Распознает команду из голосового ввода"""
@@ -24,11 +33,12 @@ def listen_command():
         return None
 
 def greeting():
-    """Приветствие"""
-    return 'Приветик!'
+    speak("Приветик!")
+    return 
 
 def play_music():
     print("Есть Босс!") 
+    speak("Есть Босс!")
     for proc in psutil.process_iter():
         if proc.name() == "Spotify.exe":
             pyautogui.press('playpause')
@@ -41,27 +51,30 @@ def play_music():
     return
 
 def open_telegram():
-    """Открытие Telegram"""
     telegram_path = os.path.expanduser("C:\\Users\\kapus\\AppData\\Roaming\\Telegram Desktop\\Telegram.exe")
     if os.path.exists(telegram_path):
         os.startfile(telegram_path)
-        return "Открываю Telegram..."
-    return "Telegram не найден. Проверьте путь к программе."
+        speak("Открываю Telegram...")
+        return
+    speak("Telegram не найден. Проверьте путь к программе.")
+    return
 
 def open_youtube():
-    """Открытие YouTube"""
     webbrowser.open("https://www.youtube.com")
-    return "Открываю YouTube..."
+    speak("Открываю YouTube...")
+    return
 
 def main():
     while True:
         query = listen_command()
         if not query:
             print("Не удалось распознать команду.")
+            speak("Не удалось распознать команду.")
             continue
         
         if query in commands_dict.get('exit_commands', []):
             print("Завершение работы...")
+            speak("Завершение работы...")
             break
         
         for command, phrases in commands_dict['commands'].items():
